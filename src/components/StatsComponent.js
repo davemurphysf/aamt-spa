f0ddd327da6321c2588940e1caf17451cdf06fea
David Murphy <dave.w.murphy@gmail.com>
2019-01-27 13:02:13 -0800

    Update stats list to be clickable

--- src/components/StatsComponent.js
+++ src/components/StatsComponent.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { actionCreators } from '../store/Stats';
+import { actionCreators } from '../store/Tweets';
 import { Col } from "react-bootstrap";
 
 class StatsComponent extends Component {
     render() {
-        const usernames = Object.keys(this.props.usernames);
+        const usernames = Object.keys(this.props.stats.usernames);
 
-        usernames.sort((a, b) => { return this.props.usernames[b] - this.props.usernames[a]; })
+        usernames.sort((a, b) => { return this.props.stats.usernames[b] - this.props.stats.usernames[a]; })
 
         const usernameComponents = [];
 
         usernames.forEach(u => {
-            usernameComponents.push(<li key={u} className="list-group-item"><span className="badge">{this.props.usernames[u]}</span>{u}</li>)
+            usernameComponents.push(<li key={u} onClick={this.requestTweets.bind(this, u)} className="list-group-item"><span className="badge" >{this.props.stats.usernames[u]}</span>{u}</li>)
         });
 
         return (
             <Col sm={4}>
                 Connected to SignalR: {' '}
-                {this.props.signalRConnected &&
+                {this.props.stats.signalRConnected &&
                     <i className="fa fa-circle fa-lg text-success" aria-hidden="true"></i>
                 }
-                {this.props.signalRConnecting &&
+                {this.props.stats.signalRConnecting &&
                     <i className="fa fa-circle fa-lg text-warning" aria-hidden="true"></i>
                 }
-                {!this.props.signalRConnected && !this.props.signalRConnecting &&
+                {!this.props.stats.signalRConnected && !this.props.stats.signalRConnecting &&
                     <i className="fa fa-circle fa-lg text-danger" aria-hidden="true"></i>
                 }
-                {this.props.signalRConnected &&
+                {this.props.stats.signalRConnected &&
                     <div>
                         <h3>Stats</h3>
                         Sign-ins: {' ' + this.props.signIns}
                         <br />
-                        Tweet Text Analyzed: {' ' + this.props.textAnalyzed}
+                        Tweet Text Analyzed: {' ' + this.props.stats.textAnalyzed}
                         <br />
-                        Tweet Images Analyzed: {' ' + this.props.picturesAnalyzed}
+                        Tweet Images Analyzed: {' ' + this.props.stats.picturesAnalyzed}
                         {usernameComponents.length > 0 &&
                             <div>
                                 <h4>Tweets Fetched</h4>
@@ -49,10 +49,16 @@ class StatsComponent extends Component {
             </Col>
         );
     }
+    requestTweets(username) {
+        if (!this.props.tweets.isLoading) {
+            this.props.requestTweets(username);
+        }
+        // e.preventDefault();
+    }
 }
 
 
 export default connect(
-    state => state.stats,
+    state => state,
     dispatch => bindActionCreators(actionCreators, dispatch)
 )(StatsComponent);
