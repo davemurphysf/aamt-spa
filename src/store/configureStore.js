c34b8f9596eed7eebaa03d9da79a202117af1a22
David Murphy <dave.w.murphy@gmail.com>
2019-01-27 12:53:44 -0800

    Update Redux devTools declaration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -17,8 +17,8 @@ export default function configureStore(history, initialState) {
 
     const enhancers = [];
     const isDevelopment = process.env.NODE_ENV === 'development';
-    if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
-        enhancers.push(window.devToolsExtension());
+    if (isDevelopment && typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) {
+        enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__());
     }
 
     const rootReducer = combineReducers({
